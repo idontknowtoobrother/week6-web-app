866ad56bf4199ffbd068c26abe3dd6267cba917d
Jakkrit Chaopron <idontknowbrotherpls@gmail.com>
2022-02-28 18:20:24 +0700

    added: products POST method, and function

--- product_backend/app.js
+++ product_backend/app.js
@@ -5,16 +5,21 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const productsRouter = require('./routes/products')
+// const bodyParser = require('body-parser')
 
 var app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
+// app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/products', productsRouter)
 
 module.exports = app;
--- product_backend/routes/products.js
+++ product_backend/routes/products.js
@@ -1,8 +1,73 @@
 const express = require('express')
 const router = express.Router()
 
+// variables
+const products = [{
+        id: 1,
+        name: 'IPad gen1 64G Wifi',
+        price: 11000.0
+    },
+    {
+        id: 2,
+        name: 'IPad gen2 64G Wifi',
+        price: 12000.0
+    },
+    {
+        id: 3,
+        name: 'IPad gen3 64G Wifi',
+        price: 13000.0
+    },
+    {
+        id: 4,
+        name: 'IPad gen4 64G Wifi',
+        price: 14000.0
+    },
+    {
+        id: 5,
+        name: 'IPad gen5 64G Wifi',
+        price: 15000.0
+    },
+    {
+        id: 6,
+        name: 'IPad gen6 64G Wifi',
+        price: 16000.0
+    },
+    {
+        id: 7,
+        name: 'IPad gen7 64G Wifi',
+        price: 17000.0
+    },
+    {
+        id: 8,
+        name: 'IPad gen8 64G Wifi',
+        price: 18000.0
+    },
+    {
+        id: 9,
+        name: 'IPad gen9 64G Wifi',
+        price: 19000.0
+    },
+    {
+        id: 10,
+        name: 'IPad gen10 64G Wifi',
+        price: 20000.0
+    }
+]
+
+
+// function
 const getProducts = (req, res, next) => {
-    
+    res.json(products)
 }
 
+const addProducts = (req, res, next) => {
+    products.push(req.body)
+    res.status(201).json(req.body)
+    console.log(products);
+}
+
+
 router.get('/', getProducts)
+router.post('/', addProducts)
+
+module.exports = router
